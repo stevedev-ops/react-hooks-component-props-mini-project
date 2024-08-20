e615f41dcfc2d17eecd2940d22a758ee2958651f
stevedev-ops <smutembei9250@gmail.com>
2024-08-20 11:50:08 +0300

    my commit

--- src/__tests__/ArticleList.test.js
+++ src/__tests__/ArticleList.test.js
@@ -28,7 +28,7 @@ test("renders a <main> element", () => {
   expect(container.querySelector("main")).toBeInTheDocument();
 });
 
-test("renders an Article component for each post passed as a prop", () => {
+test("renders a Article component for each post passed as a prop", () => {
   const { container } = render(<ArticleList posts={posts} />);
   expect(container.querySelector("main").children).toHaveLength(3);
 });
--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,11 +1,15 @@
-import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Header from "../components/Header";
 
-function Header({ name }) {
-  return (
-    <header>
-      <h1>{name}</h1>
-    </header>
-  );
-}
+test("renders a <header> element", () => {
+  const { container } = render(<Header />);
+  expect(container.querySelector("header")).toBeInTheDocument();
+});
 
-export default Header;
+test("renders a <h1> with the blog name", () => {
+  render(<Header name="Underreacted" />);
+  const h1 = screen.queryByText("Underreacted");
+  expect(h1).toBeInTheDocument();
+  expect(h1.tagName).toBe("H1");
+});
